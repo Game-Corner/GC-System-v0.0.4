fcd5bf00a173d6aac9f6d494c7764961ec851049
sedeegan <jayandseth@gmail.com>
2017-08-21 19:47:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "GCS"
+
 
 client.on('ready', () => {
   console.log('Logged in as ${client.user.username}!');
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === 
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'ping') {
   msg.reply("Pong!");
   }
 });
