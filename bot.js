5b59d780f87399a9a792de68ac6e799aa4517900
Seth <jayandseth@gmail.com>
2017-08-27 10:23:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,22 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var ignore = false
+
+client.on('message', msg => {
+  if (msg.content === 'GCMignore') {
+    ignore = true;
+    msg.reply('Channel is now ignored');
+  }
+});
+
+
 client.on('message', msg => {
   if (msg.content === 'ping') {
+    if (ignore = true) {}
+    else if (ignore = false) {
     msg.reply('Pong!');
+    }
   }
 });
 
