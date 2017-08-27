186333dc7a33099e023014426b938c09fe29f118
Seth <jayandseth@gmail.com>
2017-08-27 15:30:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const herokuPinger = require('heroku-pinger');
-const pinger = herokuPinger(SITE_URL);
+const pinger = herokuPinger(https://gc-system.herokuapp.com/);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
