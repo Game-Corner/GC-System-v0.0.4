0bfac1c01f857d95f47d8d35684b647f51670ae5
Seth <jayandseth@gmail.com>
2017-08-29 19:50:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,20 +9,6 @@ var ignore = false
 
 client.on('message', msg => {
   if (msg.content === 'GCMignore') {
-  get roles() {
-    const list = new Collection();
-    const everyoneRole = this.guild.roles.get(this.guild.id);
-
-    if (everyoneRole) list.set(everyoneRole.id, everyoneRole);
-
-    for (const roleID of this._roles) {
-      const role = this.guild.roles.get(roleID);
-      if (role) list.set(role.id, role);
-    }
-
-    return list;
-  }
-if (list.includes("@&309165526427369473") = true) {
     ignore = true;
     msg.reply('Channel is now ignored.');
     }
