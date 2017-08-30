91667cc08be0464e5cbeb413a7ebfe3c3a5a3495
Seth <jayandseth@gmail.com>
2017-08-29 19:47:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,20 +5,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-  get roles() {
-    const list = new Collection();
-    const everyoneRole = this.guild.roles.get(this.guild.id);
-
-    if (everyoneRole) list.set(everyoneRole.id, everyoneRole);
-
-    for (const roleID of this._roles) {
-      const role = this.guild.roles.get(roleID);
-      if (role) list.set(role.id, role);
-    }
-
-    return list;
-  }
-
 var ignore = false
 
 client.on('message', msg => {
@@ -36,7 +22,7 @@ client.on('message', msg => {
 
     return list;
   }
-if (list.includes('@&309165526427369473') = true) {
+if (list.includes("@&309165526427369473") = true) {
     ignore = true;
     msg.reply('Channel is now ignored.');
     }
