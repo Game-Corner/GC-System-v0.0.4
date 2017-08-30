848aaa288411892c532083ee334a33f72bb45c04
Seth <jayandseth@gmail.com>
2017-08-29 20:13:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,36 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+function roles() {
+const list = new Collection();
+    const everyoneRole = this.guild.roles.get(this.guild.id);
+
+    if (everyoneRole) list.set(everyoneRole.id, everyoneRole);
+
+    for (const roleID of this._roles) {
+      const role = this.guild.roles.get(roleID);
+      if (role) list.set(role.id, role);
+    }
+
+    return list;
+  };
+  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-var ignore = false
-
 client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+  
   if (msg.content === 'GCMignore') {
-    ignore = 'true';
-    msg.reply('Channel is now ignored.');
+    var gRole = get roles();
+    if (gRole.includes('@&309165526427369473')) {
+      msg.reply('Disabled');
     }
-
-if (msg.content === 'ping') {
-  if (ignore = 'true') {}
-  else if (ignore = 'false') {
-  msg.reply('Pong!');
- }
+  }
+  
 });
 
 client.login(process.env.token);
