8072934d0a10fc55b28fd805e8e9a0a4d533a8e6
Seth <jayandseth@gmail.com>
2017-08-31 20:50:41 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('serverRoleCreated', () => {
-  client.sendMessage("298139186819629056", role.name);
+client.on('channelCreated', () => {
+  client.sendMessage("298139186819629056", "worked");
 });
 
 client.on('message', msg => {
