96045b68749c7783179c1b6e56245eb0df4fa44c
Seth <jayandseth@gmail.com>
2017-09-01 16:28:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,26 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const channel = message.channel;
+const guildMember = message.member;
 var prefix = 'GC!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('channelCreated', () => {
-  client.sendMessage("298139186819629056", "worked");
-});
-
 client.on('message', msg => {
   if (msg.content === prefix + 'ping') {
-    msg.reply('Pong!');
+    if (msg.reply('Pong!');
+  }
+  
+  if (msg.content === prefix + 'info') {
+    msg.reply('Game Corner System is a open-sourced Discord Bot that acts as the official bot for the GC community. You can contribute to it here: https://github.com/Game-Corner/GC-System ');
+  }
+  
+  if (msg.content === prefix + 'ignore') {
+    if (guildMember.roles.has(309165526427369473) {
+        msg.reply('worked')
+    }    
   }
 });
 
