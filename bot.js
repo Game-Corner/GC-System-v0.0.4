a735fdafc0c02ff102f4c85ecc943173185ff642
Seth <jayandseth@gmail.com>
2017-08-31 20:46:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = 'GC!'
-const util = require('util')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-console.log(util.inspect(client.listeners('serverRoleCreated')));
+client.on('serverRoleCreated', () => {
+  client.sendMessage("298139186819629056", role.name);
+});
 
 client.on('message', msg => {
   if (msg.content === prefix + 'ping') {
