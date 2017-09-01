e91fd802ff4fadc54a497969669ef43455ed23e7
Seth <jayandseth@gmail.com>
2017-09-01 16:51:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,9 @@ client.on('message', msg => {
     if (guildMember.roles.has(309165526427369473)) {
         msg.reply('worked');
     }    
+    else {
+      msg.reply('did not work')
+    }
   }
 });
 
