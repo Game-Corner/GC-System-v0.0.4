fdc13fe4275c18a32813e93d34aed3fef22cc7fe
Seth <jayandseth@gmail.com>
2017-09-01 22:03:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = 'GC!';
-const ignoredChannels = new Collection();
+const ignoredChannels = new Map();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
