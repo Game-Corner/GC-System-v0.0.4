df5613ed746115ada4f20950a17b1097034b5a20
Seth <jayandseth@gmail.com>
2017-09-03 07:48:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.on('message', msg => {
             if (msg.content === 'Yes') {
               ignoredChannels.delete(msg.channel.id);
               msg.reply('Channel is now not ignored.');
-              client.on = false;
+              client.removeListener('message', msg);
             }
             else if (msg.content === 'No') {
               msg.reply('Channel is still ignored.');
