927400772e75432bfcdb9ac6bdd4d584907460e5
Seth <jayandseth@gmail.com>
2017-09-14 11:38:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,60 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var prefix = 'GC!';
+const ignoredChannels = new Map();
+
+const msg1 = msg => {
+  if (author === msg.author.id) {
+    if (msg.content === 'Yes') {
+      ignoredChannels.delete(msg.channel.id);
+      msg.reply('Channel is now not ignored.');
+      client.removeListener('message', msg1);
+    }
+    else if (msg.content === 'No') {
+      msg.reply('Channel is still ignored.');
+    }
+    else {
+      msg.reply('You did not type in the correct arguments. Please try again later.');
+    }
+  }
+};
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  
+  if (msg.content === prefix + 'ping') {
+    if (ignoredChannels.has(msg.channel.id)) {}
+    else {
+      msg.reply('Pong!');
+    }
+  }
+  
+  if (msg.content === prefix + 'info') {
+    if (ignoredChannels.has(msg.channel.id)) {}
+    else {
+      msg.reply('Game Corner System is a open-sourced Discord Bot that acts as the official bot for the GC community. You can contribute to it here: https://github.com/Game-Corner/GC-System ');
+    }
+  }
+  
+  if (msg.content === prefix + 'ignore') {
+    var guildMember = msg.member;
+    author = msg.author.id;
+    if (guildMember.hasPermission('MANAGE_GUILD', false, true, true)) {
+      if (ignoredChannels.has(msg.channel.id)) {
+        msg.reply('Would you like to stop ignoring this channel? (Yes/No)');
+        client.on('message', msg1);
+      }
+      else {
+      ignoredChannels.set(msg.channel.id, msg.channel.name);
+      msg.reply('Channel is now ignored.');
+      }
+    }
+    else {
+      msg.reply('You do not have the permissions to do this.');
+    }
   }
 });
 
