fc6c69bb0ac80be9b3aedd59aacde34bc96bc4ff
Seth <jayandseth@gmail.com>
2017-09-18 18:23:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ const quest2_1 = msg => {
     }
     else {
       prefixM = msg.content;
-      msg.reply('The Everyone prefix has been set to`' + prefix = '`.');
+      msg.reply('The Everyone prefix has been set to`' + prefix + '`.');
       client.removeListener('message', quest2_1);
     }
   }
