16d694a550936f51dc07dae9f438d3643b14b823
Seth <jayandseth@gmail.com>
2017-09-17 20:22:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,14 +68,14 @@ client.on('message', msg => {
   if (msg.content === prefix + 'commands') {
     if (ignoredChannels.has(msg.channel.id)) {}
     else {
-      msg.reply('**Commands:** \n To use these commands, type ${prefix} , then one of the commands below. __Example:__ GC!ping \n  1. `info` Provides information about Game Corner System and Game Corner \n 2. `ping` States the current ping of the bot \n **For Moderator commands, please use:** `${prefixM}commands`');
+      msg.reply('**Commands:** \n To use these commands, type `${prefix}`, then one of the commands below. __Example:__ GC!ping \n  1. `info` Provides information about Game Corner System and Game Corner \n 2. `ping` States the current ping of the bot \n **For Moderator commands, please use:** `${prefixM}commands`');
     }
   }
   
   if (msg.content === prefixM + 'commands') {
     var guildMember = msg.member;
     if (guildMember.hasPermission('MANAGE_GUILD', false, true, true)) {  
-      msg.reply('**Commands:** \n To use these commands, type ${prefixM} , then one of the commands below. __Example:__ GC!ignore \n  1. `ignore` Ignores the channel the message was sent in from all moderator commands \n **For Everyone commands, please use:** `${prefix}commands`');
+      msg.reply('**Commands:** \n To use these commands, type `${prefixM}`, then one of the commands below. __Example:__ GC!ignore \n  1. `ignore` Ignores the channel the message was sent in from all moderator commands \n **For Everyone commands, please use:** `${prefix}commands`');
     }
     else {
       msg.reply('You do not have the permissions to use these commands.');
