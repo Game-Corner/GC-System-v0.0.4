1ffaa20c6a48219fd35fd086d99728124c900102
Seth <jayandseth@gmail.com>
2017-09-24 13:17:02 -0500

    Delete clock.js

--- bot/clock.js
+++
@@ -1,9 +0,0 @@
-var CronJob = require('cron').CronJob;
-var run = require('bot/run.js');
-
-new CronJob({
-  cronTime: "0,0 6,22 * * *", // everyday, 6:00 am, 10:00 pm
-  onTick: run.start(),
-  start: true,
-  timeZone: "America/Chicago"
-});
