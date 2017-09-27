4afa6014fc7425de9fdcb56a3c07a8de35deaff0
Seth <jayandseth@gmail.com>
2017-09-27 17:39:06 -0500

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,9 +1,39 @@
+const http = require('http'); 
+const express = require('express')
+const app = express()
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var favicon = require('serve-favicon');
+var path = require('path');
 var ignoredChannels = new Map();
 var moderationRoles = new Map();
 var prefix = 'GC!';
 var prefixM = 'GCm!';
+var date = new Date();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.set('views', '/app/site');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+/*
+app.use(favicon(path.join('site', 'files', 'favicon.ico')));
+*/
+
+if (6 <= date.getHours() <= 22) {
+  setInterval(function() {
+    http.get("http://gc-system.herokuapp.com/");
+  }, 300000); // every 5 minutes (300000)
+};
+
 
 const quest = msg => {
   if (author === msg.author.id) {
