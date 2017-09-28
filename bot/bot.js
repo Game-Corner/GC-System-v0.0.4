5c6b8592caeaeffd762212e686550ddc352a64bf
Seth <jayandseth@gmail.com>
2017-09-28 11:37:42 -0500

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -35,19 +35,19 @@ setInterval(function() {
   }
 }, 300000); // every 5 minutes (300000)
 
-const client = new Client({
+const con = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
-client.connect();
+con.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+con.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
+  con.end();
 });
 
 const quest = msg => {
