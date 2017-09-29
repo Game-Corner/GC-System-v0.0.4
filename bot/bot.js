d4f4fa1b80b4bcb5e894b3d0605ade5781a24b49
Seth <jayandseth@gmail.com>
2017-09-29 11:00:23 -0500

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -47,7 +47,7 @@ con.query('SELECT table_schema,table_name FROM information_schema.tables;', (err
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
+  con.end();
 });
 
 const quest = msg => {
