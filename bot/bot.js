ccef4e7dce233f5180e3a803765abbb2b8a23684
Seth <jayandseth@gmail.com>
2017-09-29 10:28:19 -0500

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -43,7 +43,7 @@ const con = new Client({
 con.connect();
 
 con.query('CREATE DATABASE gc-system OWNER process.env.DATABASE_USER', (err, res) => {
-  if (err) throw err;
+  if err throw err;
   for (let tables of res.tables) {
     console.log(JSON.stringify(tables));
   }
